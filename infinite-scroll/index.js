513f3122f8256702ec8ffe2f5f72fc18c0aed243
Archit <architm@Archits-MacBook-Air-2.local>
2021-08-03 22:54:47 -0400

    Completed infinite scroll project

---
+++ infinite-scroll/index.js
@@ -0,0 +1,84 @@
+const imageContainer = document.getElementById("image-container");
+const loader = document.getElementById("loader");
+
+let ready = false;
+let imagesLoaded = 0;
+let totalImages = 0;
+let photosArray = [];
+let initialLoad = true;
+
+// Unsplash API
+const count = 5;
+const apiKey = "gTCUQwTrghMAZcplY_Gl3cSm5DOogMNow4cLN7-pNo4";
+const apiUrl = `https://api.unsplash.com/photos/random/?client_id=${apiKey}&count=${count}`;
+
+//  Check loading
+const imageLoaded = () => {
+  imagesLoaded++;
+  if (imageLoaded === totalImages) {
+    ready = true;
+    loader.hidden = true;
+    initialLoad = false;
+    count = 30;
+  }
+};
+
+// Helper Functions
+const setAttributes = (element, attributes) => {
+  for (const key in attributes) {
+    element.setAttribute(key, attributes[key]);
+  }
+};
+
+// Display Photos
+const displayPhotos = () => {
+  imagesLoaded = 0;
+  totalImages = photosArray.length;
+  photosArray.forEach((photo) => {
+    const item = document.createElement("a");
+    setAttributes(item, {
+      href: photo.links.html,
+      target: "_blank",
+    });
+
+    // Create img
+    const img = document.createElement("img");
+    setAttributes(img, {
+      src: photo.urls.regular,
+      alt: photo.alt_description,
+      title: photo.alt_description,
+    });
+
+    img.addEventListener("load", imageLoaded);
+
+    // Put img inside a
+    item.appendChild(img);
+    imageContainer.appendChild(img);
+  });
+};
+
+// Get Photos
+const getPhotos = async () => {
+  try {
+    const response = await fetch(apiUrl);
+    photosArray = await response.json();
+    displayPhotos();
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+// Load photos while scrolling
+window.addEventListener("scroll", () => {
+  if (
+    window.innerHeight + window.scrollY >= document.body.offsetHeight - 1000 &&
+    ready
+  ) {
+    ready = false;
+    getPhotos();
+  }
+});
+
+window.addEventListener("load", imageLoaded);
+
+getPhotos();
